410d7e5a943f8fe30010bae3594b2686ce230784
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-12-19 10:58:28 -0700

    added schema dir and files

--- server/server.js
+++ server/server.js
@@ -1,12 +1,50 @@
 const express = require('express');
+
+// to use Playground
+const { ApolloServerPluginLandingPageGraphQLPlayground } = require("apollo-server-core");
+// install Apollo server
 const { ApolloServer } = require("apollo-server-express");
 const path = require('path');
+
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require("./schemas")
+
+// import middleware
+const { authMiddleware } = require("./utils/auth");
+
+// db connection 
 const db = require('./config/connection');
+
 const routes = require('./routes');
 
+// express server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// apollo server
+const startServer = async () => {
+  // create a new Apollo server and pass in our schema data
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+    context: authMiddleware,
+  });
+
+  // start the apollo server
+  await server.start();
+
+  // integrate our Apollo server with Express app as middleware
+  server.applyMiddleware({ app });
+
+  // log where we can go to test out GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+// initialize the apollo server
+startServer();
+
+// middleware parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -17,6 +55,14 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
+// //get all
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
+
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
