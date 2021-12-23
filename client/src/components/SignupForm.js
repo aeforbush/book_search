a510808e8216bfdd6034f6b489b807dc671482c7
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-12-23 09:33:16 -0700

    minor tweaks

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -30,7 +30,7 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const response = await createUser({variables: userFormData});
 
       if (!response.ok) {
         throw new Error('something went wrong!');
